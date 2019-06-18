507b16f5ccefc28fec0a50ea1340b5c5d397f7a0
Francis Milandou <mil15083@byui.edu>
2019-06-17 19:42:34 -0600

    Add project files.

---
+++ ScriptureJournal/Models/SeedData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace ScriptureJournal.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ScriptureJournalContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ScriptureJournalContext>>()))
+            {
+                // Look for any Scriptures.
+                if (context.Book.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Book.AddRange(
+                    new Journal
+                    {
+                        Book = "Genesis",
+                        Chapter = 1,
+                        Verse = 1,
+                        Note = "This begain it all!",
+                        DateAdded = DateTime.Parse("2018-11-5")
+                    },
+
+                    new Journal
+                    {
+                        Book = "1 Nephi",
+                        Chapter = 1,
+                        Verse = 1,
+                        Note = "The first verse in the Book of Mormon.",
+                        DateAdded = DateTime.Parse("2018-11-5")
+                    }
+
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
